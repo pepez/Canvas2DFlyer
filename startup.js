ce41db1260f7cb3a756a89606fc847f2b1afe8a5
pepez <petteri.hietavirta@gmail.com>
2011-01-31 18:43:38 +0000

    Fixed license text.

--- startup.js
+++ startup.js
@@ -1,9 +1,9 @@
-// Canvas2DFlyer
+// startup.js
 // Copyright (C) 2011  Petteri Hietavirta
 //
 // This file is part of Canvas2DFlyer.
 // 
-// ReelSnail is free software: you can redistribute it and/or modify
+// Canvas2DFlyer is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
